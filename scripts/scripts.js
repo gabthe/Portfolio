1842bf86c34d9cf7cfbfbade9119ef6064af05bd
Gabriel Theodoro Ferreira <gabthefer1@gmail.com>
2022-06-08 06:54:35 -0300

    Scripts bugfix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,10 +2,10 @@ const btn = document.querySelector('#minimize-small')
 const skills = document.querySelector('.all-skills')
 
 btn.addEventListener('click', function(){
-    if(skills.style.display === 'block') {
-        skills.style.display = 'none';
-    } else {
+    if(skills.style.display === 'none') {
         skills.style.display = 'block';
+    } else {
+        skills.style.display = 'none';
     }
 })
 
@@ -13,9 +13,9 @@ const btn2 = document.querySelector('#minimize-small2')
 const container = document.querySelector('.container-projects')
 
 btn2.addEventListener('click', function() {
-    if(container.style.display === 'flex') {
-        container.style.display = 'none'
-    } else {
+    if(container.style.display === 'none') {
         container.style.display = 'flex'
+    } else {
+        container.style.display = 'none'
     }
 })
